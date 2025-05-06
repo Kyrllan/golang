61476a21e8e8aa008f6c45ec8d3228f47065bf4b
Kyrllan Nogueira <kyrllannogueira@iMac-de-Kyrllan.local>
2025-05-06 16:59:17 -0300

    readme created

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo o arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("kyrllan@hotmail.com")
+	fmt.Println(erro)
 }
