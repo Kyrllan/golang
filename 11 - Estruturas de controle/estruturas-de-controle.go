4b8091c9427ea65c817015cc34ad61b5c3cd46ea
Kyrllan Nogueira <kyrllan@hotmail.com>
2025-05-08 19:58:00 -0300

    estruturas de controle

---
+++ 11 - Estruturas de controle/estruturas-de-controle.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	numero := 10
+
+	if numero > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor ou igual a 15")
+	}
+
+	if outroNumero := numero; outroNumero > 0 { //Inicialização de variável dentro do if
+		fmt.Println("Número é maior que 0")
+	}
+}
