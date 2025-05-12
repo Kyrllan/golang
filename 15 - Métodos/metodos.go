6f1bfd3dd817eb33e6a451684715e0253c64b4d0
Kyrllan Nogueira <kyrllan@hotmail.com>
2025-05-12 20:44:28 -0300

    metodos

---
+++ 15 - Métodos/metodos.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome  string
+	idade uint8
+}
+
+func (u usuario) salvar() {
+	fmt.Printf("Salvando os dados do Usuário %s no banco de dados\n", u.nome)
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade >= 18
+}
+
+func (u *usuario) fazerAniversario() {
+	u.idade++
+}
+
+func main() {
+	usuario1 := usuario{"Usuário 1", 20}
+	fmt.Println(usuario1)
+	usuario1.salvar()
+	maiorDeIdade := usuario1.maiorDeIdade()
+	fmt.Println(maiorDeIdade)
+	usuario1.fazerAniversario()
+	fmt.Println(usuario1.idade)
+
+}
