b80c28d62bcaedd3603626b49ababe2e40a9e392
Kyrllan Nogueira <kyrllan@hotmail.com>
2025-05-19 15:48:40 -0300

    concorrencia

---
+++ 18 - Concorrencia/goroutines/goroutines.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//CONCORRÊNCIA != PARALELISMO
+	escrever("Olá mundo")          //essa função vai ficar rodando infinitamente
+	escrever("Programando em Go!") //essa função vai ficar rodando infinitamente
+}
+
+func escrever(texto string) {
+	for {
+		fmt.Println(texto)
+	}
+}
