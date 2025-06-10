085d02bf018275b7d9bc72bcabac3e985e7465ac
Kyrllan Nogueira de Souza <kyrllan@hotmail.com>
2025-06-10 11:53:10 -0300

    removido rota de produtos

--- 21 - HTTP/http.go
+++ 21 - HTTP/http.go
@@ -24,9 +24,5 @@ func main() {
 		w.Write([]byte("Bem vindo a página de usuários"))
 	})
 
-	http.HandleFunc("/produtos", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Bem vindo a página de produtos"))
-	})
-
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
