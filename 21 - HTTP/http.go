182245a72adb1e0ef9754dddbfd97b40942d1c9c
Kyrllan Nogueira de Souza <kyrllan@hotmail.com>
2025-06-10 11:33:48 -0300

    Metodos Http

---
+++ 21 - HTTP/http.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// HTTP é um protocolo de comunicação - base da comunicação Web
+
+	// Cliente (Faz requisição) - Servidor (Processa requisição e envia resposta)
+
+	// Request - Response
+
+	// Rotas
+	// URI - Identificador do Recurso
+	// Método - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá Mundo"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
