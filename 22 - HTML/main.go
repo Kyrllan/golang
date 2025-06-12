0654aed25ca54f81122e84827d14e6e65a239ef8
Kyrllan Nogueira de Souza <kyrllan@hotmail.com>
2025-06-12 09:23:01 -0300

    renderizando template html

--- 22 - HTML/main.go
+++ 22 - HTML/main.go
@@ -9,12 +9,20 @@ import (
 
 var templates *template.Template
 
+type usuario struct {
+	Nome  string
+	Email string
+}
+
 func main() {
 
 	templates = template.Must(template.ParseGlob("*.html"))
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "home.html", nil)
+
+		usuario := usuario{"Kyrllan", "kyrllan@hotmail.com"}
+
+		templates.ExecuteTemplate(w, "home.html", usuario)
 	})
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
