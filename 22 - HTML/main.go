c71b3343446e44b21ee249aa6f6ee6e63e804bec
Kyrllan Nogueira de Souza <kyrllan@hotmail.com>
2025-06-12 09:17:33 -0300

    html

--- 22 - HTML/main.go
+++ 22 - HTML/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates *template.Template
+
 func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-
+		templates.ExecuteTemplate(w, "home.html", nil)
 	})
-
+	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
